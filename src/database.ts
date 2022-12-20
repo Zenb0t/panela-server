0401e264b4aa2703205443629fe717c2d5cdc271
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2022-12-19 22:21:10 -0800

    minor refactor and clean up

--- src/database.ts
+++ src/database.ts
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import sanitizedConfig from "./config";
 
 async function initDB(uri: string) {
     console.log("Connecting to MongoDB...");
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,5 @@
 import express from 'express';
-import { auth } from 'express-openid-connect';
-import { requiresAuth } from 'express-openid-connect'; //add when required to protect routes
+import { auth } from 'express-oauth2-jwt-bearer';
 import sanitizedConfig from './config';
 import recipeRouter from './routes/api/recipes';
 import initDB from './database';
@@ -13,37 +12,21 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.listen(sanitizedConfig.PORT, () => {
-  console.log(`⚡️[server]: Server is running at ${sanitizedConfig.BASE_URL}`);
+  console.log(`⚡️[server]: Server is running at localhost:${sanitizedConfig.PORT}`);
 });
 
+// auth middleware config
 const config = {
-  authRequired: false,
-  auth0Logout: true,
-  secret: sanitizedConfig.SESSION_SECRET,
-  baseURL: sanitizedConfig.BASE_URL,
-  clientID: sanitizedConfig.AUTH0_CLIENT_ID,
   issuerBaseURL: sanitizedConfig.AUTH0_DOMAIN,
+  audience: sanitizedConfig.AUDIENCE,
 };
 
 //initialize database
 initDB(sanitizedConfig.URI_MONGODB);
 
-// auth router attaches /login, /logout, and /callback routes to the baseURL
-app.use(auth(config));
-
 // allow pre-flight cors requests
 app.use(cors());
 
-// require api routes
-app.use('/api',recipeRouter);
-app.use('/api',ingredientRouter);
-
-// req.isAuthenticated is provided from the auth router
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
-
-// example of a protected route
-app.get('/profile', requiresAuth(), (req, res) => {
-  res.send(JSON.stringify(req.oidc.user));
-});
+// api routes, with auth middleware
+app.use('/api', auth(config), recipeRouter);
+app.use('/api', auth(config), ingredientRouter);
