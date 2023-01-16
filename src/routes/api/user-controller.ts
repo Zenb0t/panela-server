ca7d0cb546dc11ee4b15c88e96e42c586b4b31d9
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-01-15 22:29:12 -0800

    name change and new functions

--- src/routes/api/user-controller.ts
+++ src/routes/api/user-controller.ts
@@ -1,4 +1,4 @@
-import { User, UserModel } from "./models/user";
+import { User, UserModel } from "../../models/user";
 
 //TODO: Transfer control from the users route to the user manager
 
@@ -9,12 +9,14 @@ export class UserManager {
     private static instance: UserManager;
     private userMap: Map<string, User>;
 
+    // Private constructor to prevent instantiation
     private constructor() {
         // Initialize the user map
         this.userMap = new Map<string, User>();
         this.initializeUserMap();
     }
 
+    // Implement Singleton
     static getInstance(): UserManager {
         if (!UserManager.instance) {
             UserManager.instance = new UserManager();
@@ -26,6 +28,23 @@ export class UserManager {
         return this.userMap;
     }
 
+    /** Adds a user to the user map, if the email already exists, updates the value of it */
+    addUser(user: User) {
+        this.userMap.set(user.email, user);
+    }
+
+    /** Removes a user from the user map */
+
+    removeUser(user: User) {
+        this.userMap.delete(user.email);
+    }
+
+    /** Returns the user with the given email */
+    getUser(email: string): User | undefined {
+        return this.userMap.get(email);
+    }
+
+    /** Fetch all users from the DB and populates the user map*/
     async initializeUserMap() {
         const users = await UserModel.find();
         users.forEach((user) => {
