c5ed1adc5fc484e4347fdb615728653efd78de96
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-11-26 11:54:23 -0800

    Add global middlewares

---
+++ src/middleware.ts
@@ -0,0 +1,35 @@
+import express, { RequestHandler } from "express";
+import { auth } from "express-oauth2-jwt-bearer";
+import cors from "cors";
+import sanitizedConfig from "./config";
+import { handleError } from "./utils/errorHandler";
+
+export const authMiddleware = auth({
+  issuerBaseURL: sanitizedConfig.AUTH0_DOMAIN,
+  audience: sanitizedConfig.AUDIENCE,
+});
+
+export const validateUser: RequestHandler = (req, res, next) => {
+  if (!req.query.userId)
+    return res
+      .status(400)
+      .send({ message: "Query parameter userId is required" });
+  next();
+};
+
+/**
+ * Express Router for global middleware configurations.
+ * Handles JSON parsing, urlencoded parsing, and CORS.
+ * @type {express.Router}
+ */
+const globalMiddleware = express.Router();
+
+globalMiddleware.use(express.json());
+globalMiddleware.use(express.urlencoded({ extended: true }));
+
+globalMiddleware.use(cors());
+
+// Error handler middleware, keep as the last middleware to catch all errors
+globalMiddleware.use(handleError);
+
+export default globalMiddleware;
