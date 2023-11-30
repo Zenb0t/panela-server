c9949d7f4d72b07ef62f98c3a803c22116964915
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-11-30 14:35:39 -0800

    Lint

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,32 +1,34 @@
-import fs from 'fs';
-import path from 'path';
-import winston from 'winston';
+import fs from "fs";
+import path from "path";
+import winston from "winston";
 
 // Ensure that the logs directory exists
-const logsDir = path.join(__dirname, '../..', 'logs');
+const logsDir = path.join(__dirname, "../..", "logs");
 if (!fs.existsSync(logsDir)) {
-    fs.mkdirSync(logsDir);
+  fs.mkdirSync(logsDir);
 }
 
 // Logger configuration
 const logConfiguration = {
-    'transports': [
-        new winston.transports.Console({
-            level: 'info',
-            format: winston.format.combine(
-                winston.format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
-                winston.format.printf(info => `${info.timestamp} ${info.level}: ${info.message}`)
-            )
-        }),
-        new winston.transports.File({
-            level: 'error',
-            filename: path.join(logsDir, 'errors.log'),
-            format: winston.format.combine(
-                winston.format.timestamp({ format: 'MMM-DD-YYYY HH:mm:ss' }),
-                winston.format.json()
-            )
-        })
-    ]
+  transports: [
+    new winston.transports.Console({
+      level: process.env.NODE_ENV === "development" ? "debug" : "info",
+      format: winston.format.combine(
+        winston.format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+        winston.format.printf(
+          (info) => `${info.timestamp} ${info.level}: ${info.message}`
+        )
+      ),
+    }),
+    new winston.transports.File({
+      level: "error",
+      filename: path.join(logsDir, "errors.log"),
+      format: winston.format.combine(
+        winston.format.timestamp({ format: "MMM-DD-YYYY HH:mm:ss" }),
+        winston.format.json()
+      ),
+    }),
+  ],
 };
 
 const logger = winston.createLogger(logConfiguration);
