935bbcf80db4ab86c404e9851901390bf9245fb5
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2023-12-03 20:26:53 -0800

    Add tests to ingredient e ndpoint

--- src/ingredients/tests/fixtures.ts
+++ src/ingredients/tests/fixtures.ts
@@ -7,3 +7,27 @@ export const dummyIngredient: Ingredient = {
     costPerUnit: 0.25,
     _id: "testId"
 }
+
+export const dummyIngredients: Ingredient[] = [
+    {
+        amount: 1,
+        measuringUnit: MeasuringUnit.CUP,
+        name: "Flour",
+        costPerUnit: 0.25,
+        _id: "testId1"
+    },
+    {
+        amount: 1,
+        measuringUnit: MeasuringUnit.CUP,
+        name: "Sugar",
+        costPerUnit: 0.25,
+        _id: "testId2"
+    },
+    {
+        amount: 1,
+        measuringUnit: MeasuringUnit.CUP,
+        name: "Eggs",
+        costPerUnit: 0.25,
+        _id: "testId3"
+    },
+]
---
+++ src/ingredients/tests/serializeAllIngredients.test.ts
@@ -0,0 +1,45 @@
+import { Request, Response, NextFunction } from 'express';
+import { serializeAllIngredients } from '../controller';
+import { getAllIngredients } from '../dao';
+import { dummyIngredients } from './fixtures'; // Ensure this array of ingredients is defined
+import { handleError } from "../../utils/errorHandler";
+
+jest.mock("../dao");
+jest.mock("../../utils/errorHandler");
+
+describe('serializeAllIngredients Middleware', () => {
+  const mockResponse = (): Response => {
+    const res = {} as Response;
+    res.status = jest.fn().mockReturnValue(res);
+    res.send = jest.fn().mockReturnValue(res);
+    return res;
+  };
+
+  const mockNext: NextFunction = jest.fn();
+  const mockRequest = {} as Request; // Assuming no specific request parameters are needed
+
+  it('should retrieve all ingredients and send a 200 response', async () => {
+    (getAllIngredients as jest.Mock).mockResolvedValue(dummyIngredients);
+
+    const req = mockRequest;
+    const res = mockResponse();
+
+    await serializeAllIngredients(req, res, mockNext);
+
+    expect(getAllIngredients).toHaveBeenCalled();
+    expect(res.status).toHaveBeenCalledWith(200);
+    expect(res.send).toHaveBeenCalledWith(dummyIngredients);
+  });
+
+  it('should call handleError on failure to retrieve ingredients', async () => {
+    const mockError = new Error('Fetch failed');
+    (getAllIngredients as jest.Mock).mockRejectedValue(mockError);
+
+    const req = mockRequest;
+    const res = mockResponse();
+
+    await serializeAllIngredients(req, res, mockNext);
+
+    expect(handleError).toHaveBeenCalledWith(mockError, req, res, mockNext);
+  });
+});
