f2f702a630087d66f672f02b7a1a6e56c0b3f76b
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2024-01-02 15:53:02 -0800

    Add masterPattern to combinedPattern in patterns.ts

--- src/utils/patterns.ts
+++ src/utils/patterns.ts
@@ -6,6 +6,8 @@ export const multiplicationPattern = /(\d+ x \d+)/;
 export const unicodeFractionPattern = /([¼½¾⅓⅔⅛⅜⅝⅞])/;
 export const wholeNumberPattern = /(\d+)/;
 
+export const masterPattern = /(\d+(?:\.\d+)?\/\d+|\d+-\d+|\d+\.\d+|\d+\s*\d*\/\d*|\d+ x \d+|[¼½¾⅓⅔⅛⅜⅝⅞]|\d+)/;
+
 export const combinedPattern = new RegExp(
-	`${fractionPattern.source}|${rangePattern.source}|${decimalPattern.source}|${mixedFractionPattern.source}|${multiplicationPattern.source}|${unicodeFractionPattern.source}|${wholeNumberPattern.source}`
+	`(${fractionPattern.source}|${rangePattern.source}|${decimalPattern.source}|${mixedFractionPattern.source}|${multiplicationPattern.source}|${unicodeFractionPattern.source}|${wholeNumberPattern.source})`,
 );
