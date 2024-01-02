55cb7ad181e05e48c7ccd40278fb945b96668797
Zenb0t <46074517+Zenb0t@users.noreply.github.com>
2024-01-02 14:44:10 -0800

    update parseIngredient

--- src/scrapper/scrapper.ts
+++ src/scrapper/scrapper.ts
@@ -8,7 +8,19 @@ import {
 	MEASURING_UNITS_MAPPING,
 	MEASURING_UNITS,
 	MeasuringUnit,
+	ParsedIngredient,
 } from "../types/ingredient";
+import {
+	combinedPattern,
+	decimalPattern,
+	fractionPattern,
+	mixedFractionPattern,
+	multiplicationPattern,
+	rangePattern,
+	unicodeFractionPattern,
+	wholeNumberPattern,
+} from "../utils/patterns";
+import { fractionToDecimal, multiplyExpression } from "../utils/math";
 
 // TODO Extract this to a separate file
 const context: jsonld.ContextDefinition = {
@@ -18,6 +30,7 @@ const context: jsonld.ContextDefinition = {
 	image: "http://schema.org/image",
 	recipeYield: "http://schema.org/recipeYield",
 	recipeIngredient: "http://schema.org/recipeIngredient",
+	ingredients: "http://schema.org/ingredients",
 	recipeInstructions: {
 		"@id": "http://schema.org/recipeInstructions",
 		"@type": "@id",
@@ -120,95 +133,251 @@ async function scanForjsonLD(document: Document): Promise<Recipe | null> {
 
 export async function extractRecipeData(data: jsonld.NodeObject): Promise<any> {
 	console.log("Extracting recipe data...");
-	// transverse the JSON-LD object to find the recipe data
-	// assuming the recipe data inside the @graph array
-	const graph = data["@graph"];
-	if (!graph) {
-		throw new Error("No data found");
-	} else if (Array.isArray(graph)) {
+	const type = data["@type"];
+	let recipe;
+
+	// There are 3 cases:
+	// 1. The data is a single object with the type "http://schema.org/Recipe"
+	// 2. The data is an array of objects with the type "http://schema.org/Recipe"
+	// 3. The data is an array of objects with the type "http://schema.org/Recipe" and other types
+
+	if (type === "http://schema.org/Recipe") {
+		console.debug("Type is a single object");
+		recipe = data;
+	} else if (
+		Array.isArray(type) &&
+		type.includes("http://schema.org/Recipe")
+	) {
+		console.debug("Type is an array");
+		recipe = data;
+	} else if (data["@graph"] && Array.isArray(data["@graph"])) {
 		console.log("Graph is an array");
-		const recipe = graph.find(
+		const graph = data["@graph"];
+		recipe = graph.find(
 			(item: any) => item["@type"] === "http://schema.org/Recipe"
 		);
 		if (!recipe) {
 			throw new Error("No recipe found");
 		}
-		// extract the recipe data
-		// console.log("Recipe found:", recipe);
-		const newRecipe = {
-			name: recipe.name,
-			description: recipe.description,
-			recipeYield: recipe.recipeYield,
-			recipeIngredient: recipe.recipeIngredient,
-			recipeInstructions: recipe.recipeInstructions,
-			datePublished: recipe.datePublished,
-			prepTime: recipe.prepTime,
-			cookTime: recipe.cookTime,
-			totalTime: recipe.totalTime,
-			keywords: recipe.keywords,
-			recipeCategory: recipe.recipeCategory,
-			recipeCuisine: recipe.recipeCuisine,
-			suitableForDiet: recipe.suitableForDiet,
-			aggregateRating: recipe.aggregateRating,
-			nutrition: recipe.nutrition,
-			mentions: recipe.mentions,
-			isPartOf: recipe.isPartOf,
-			mainEntityOfPage: recipe.mainEntityOfPage,
-			author: recipe.author,
-			image: recipe.image,
-			url: recipe.url,
-		};
-		// console.dir(newRecipe, { depth: null });
-		if (newRecipe.recipeIngredient) {
-			console.log("Recipe ingredients found");
-			const ingredients = newRecipe.recipeIngredient as string[];
-			ingredients.forEach((ingredient) => {
-				const parsedIngredient = parseIngredient(ingredient);
-				console.log(parsedIngredient);
-				ingredient = parsedIngredient;
-			});
-			newRecipe.recipeIngredient = ingredients;
-		}
-		return newRecipe;
+	} else {
+		console.debug(data);
+		throw new Error("Invalid data format");
 	}
+
+	console.log(recipe);
+
+	const newRecipe = {
+		name: recipe.name,
+		description: recipe.description,
+		recipeYield: recipe.recipeYield,
+		recipeIngredient: recipe.recipeIngredient,
+		ingredients: recipe.ingredients,
+		recipeInstructions: recipe.recipeInstructions,
+		datePublished: recipe.datePublished,
+		prepTime: recipe.prepTime,
+		cookTime: recipe.cookTime,
+		totalTime: recipe.totalTime,
+		keywords: recipe.keywords,
+		recipeCategory: recipe.recipeCategory,
+		recipeCuisine: recipe.recipeCuisine,
+		suitableForDiet: recipe.suitableForDiet,
+		aggregateRating: recipe.aggregateRating,
+		nutrition: recipe.nutrition,
+		mentions: recipe.mentions,
+		isPartOf: recipe.isPartOf,
+		mainEntityOfPage: recipe.mainEntityOfPage,
+		author: recipe.author,
+		image: recipe.image,
+		url: recipe.url,
+	};
+
+	if (newRecipe.recipeIngredient) {
+		console.log("Recipe ingredients found");
+		const ingredients = newRecipe.recipeIngredient as string[];
+		newRecipe.recipeIngredient = ingredients.map((ingredient) => {
+			const parsedIngredient = parseIngredient(ingredient);
+			console.log(parsedIngredient);
+			return parsedIngredient;
+		});
+	} else if (newRecipe.ingredients) {
+		// Some websites use the superseded property "ingredients" instead of "recipeIngredient"
+		console.log("Ingredients found");
+		const ingredients = newRecipe.ingredients as string[];
+		newRecipe.ingredients = ingredients.map((ingredient) => {
+			const parsedIngredient = parseIngredient(ingredient);
+			console.log(parsedIngredient);
+			return parsedIngredient;
+		});
+	}
+
+	return newRecipe;
 }
 
+/**
+ * Parses an ingredient string and extracts the amount, unit, and name of the ingredient.
+ * @param ingredient - The ingredient string to parse.
+ * @returns An object containing the parsed ingredient data.
+ */
 function parseIngredient(ingredient: string): any {
-    const options = {
-      includeScore: true,
-      threshold: 0.6,
-    };
-    const fuse = new Fuse(MEASURING_UNITS, options);
-  
-    // Match amount and unit
-    const pattern = /(\d+\/\d+|\d+\s*\d*\/\d*|\d+-\d+|\d+)?\s*([a-zA-Z]*)/;
-    const match = pattern.exec(ingredient);
-    const amount = match ? match[1] : undefined;
-    const possibleUnit = match ? match[2] : "";
-  
-    // Match unit
-    // First try to match the whole string with the units
+	const options = {
+		includeScore: true,
+		threshold: 0.5,
+	};
+	const fuse = new Fuse(MEASURING_UNITS, options);
 
-    const unitMatch = fuse.search(possibleUnit);
-    console.log("Possible unit:", possibleUnit);
-    console.log("Unit match:", unitMatch);
-    let unit = "unknown";
-    if (unitMatch.length > 0) {
-      unit = unitMatch[0].item;
-    } else {
-      unit = possibleUnit;
-    }
-  
-    // Match name
-    const name = ingredient
-      .replace(pattern, "")
-      .trim(); // The rest is name
-  
-    const ingredientData = {
-      amount: amount,
-      unit: unit,
-      name: name,
-    };
-  
-    return ingredientData;
-  }
+	// Match amount
+	const amountPattern =
+		/(\d+(?:\.\d+)?\/\d+|\d+-\d+|\d+\.\d+|\d+\s*\d*\/\d*|\d+ x \d+|[¼½¾⅓⅔⅛⅜⅝⅞]|\d+)/;
+	const matchAmount = amountPattern.exec(ingredient);
+	const amount = matchAmount ? matchAmount[1] : undefined;
+
+	// Match unit
+	// First try to match the whole string with the existing units using Regex
+	const unitPattern = new RegExp(
+		"(?<=\\d)\\s*(" + MEASURING_UNITS.join("|") + ")\\b",
+		"i"
+	);
+	const matchUnit = unitPattern.exec(ingredient);
+	let possibleUnit = matchUnit ? matchUnit[0] : undefined;
+
+	if (!possibleUnit) {
+		// If no match, try to match the unit with the existing units using Fuse.js for fuzzy search
+		const unitMatch = fuse.search(ingredient);
+		console.log(unitMatch);
+		if (unitMatch.length > 0) {
+			possibleUnit = unitMatch[0].item;
+		} else {
+			possibleUnit = MEASURING_UNITS_MAPPING.piece;
+		}
+	}
+
+	// Match name
+	const name = ingredient
+		.replace(amountPattern, "")
+		.replace(possibleUnit ? possibleUnit : "", "")
+		.trim();
+
+	// Normalize amount
+
+	const normalizedAmount = amount ? normalizeAmount(amount) : undefined;
+
+	const ingredientData: ParsedIngredient = {
+		amount: normalizedAmount,
+		unit: possibleUnit.toLowerCase(),
+		name: name,
+		source: ingredient,
+	};
+
+	return ingredientData;
+}
+
+// /**
+//  * Normalizes the amount by converting it to a decimal number.
+//  *
+//  * @param amount - The amount to be normalized, which can be a whole number, fraction, mixed fraction, or range.
+//  * @returns The normalized amount as a decimal number.
+//  */
+// function normalizeAmount(amount: string): number {
+// 	// Sometimes amounts may be written as fractions, e.g. 1/2, 1/4, 3/4, etc. or unicode fractions, e.g. ¼, ½, ¾, etc.
+// 	// Convert unicode fractions to fractions
+
+// 	// Convert fractions to decimals by evaluating the string
+// 	const fractionToDecimal = (fraction: string) => {
+// 		return eval(fraction);
+// 	};
+
+// 	// If the amount is a unicode fraction, convert it to a fraction
+// 	const unicodeFractions: { [key: string]: string } = {
+// 		"\u00BC": "1/4", // ¼
+// 		"\u00BD": "1/2", // ½
+// 		"\u00BE": "3/4", // ¾
+// 		"\u2153": "1/3", // ⅓
+// 		"\u2154": "2/3", // ⅔
+// 		"\u215B": "1/8", // ⅛
+// 		"\u215C": "3/8", // ⅜
+// 		"\u215D": "5/8", // ⅝
+// 		"\u215E": "7/8", // ⅞
+// 	};
+
+// 	if (unicodeFractions[amount]) {
+// 		amount = unicodeFractions[amount];
+// 	}
+
+// 	// If the amount is a mixed fraction, convert it to a decimal
+// 	if (amount.includes(" ")) {
+// 		const mixedFraction = amount.split(" ");
+// 		const wholeNumber = Number(mixedFraction[0]);
+// 		const fraction = mixedFraction[1];
+// 		const fractionDecimal = fractionToDecimal(fraction);
+// 		amount = (wholeNumber + fractionDecimal).toString();
+// 	}
+
+// 	// If the amount is a fraction, convert it to a decimal
+// 	if (amount.includes("/")) {
+// 		amount = fractionToDecimal(amount).toString();
+// 	}
+
+// 	// If the amount is a range, convert it to the average
+// 	if (amount.includes("-")) {
+// 		const range = amount.split("-");
+// 		const average = (Number(range[0]) + Number(range[1])) / 2;
+// 		amount = average.toString();
+// 	}
+
+// 	// If the amount is expressed as "number x number", evaluate it
+// 	if (amount.includes(" x ")) {
+// 		amount = eval(amount.replace(" x ", "*")).toString();
+// 	}
+
+// 	return Number(amount);
+// }
+
+function normalizeAmount(amount: string): number {
+	// Convert unicode fractions to normal fractions
+	amount = amount.replace(unicodeFractionPattern, (match) => {
+		const unicodeFractions: { [key: string]: string } = {
+			"\u00BC": "1/4", // ¼
+			"\u00BD": "1/2", // ½
+			"\u00BE": "3/4", // ¾
+			"\u2153": "1/3", // ⅓
+			"\u2154": "2/3", // ⅔
+			"\u215B": "1/8", // ⅛
+			"\u215C": "3/8", // ⅜
+			"\u215D": "5/8", // ⅝
+			"\u215E": "7/8", // ⅞
+		};
+		return unicodeFractions[match];
+	});
+
+	// Convert mixed fractions to decimal
+	if (mixedFractionPattern.test(amount)) {
+		console.log("Mixed fraction");
+		console.log(amount);
+		const [whole, fraction] = amount.split(" ");
+		return parseInt(whole) + fractionToDecimal(fraction);
+	}
+
+	// Convert simple fractions to decimal
+	if (fractionPattern.test(amount)) {
+		return fractionToDecimal(amount);
+	}
+
+	// Convert ranges to their average
+	if (rangePattern.test(amount)) {
+		const [min, max] = amount.split("-").map(Number);
+		return (min + max) / 2;
+	}
+
+	// Handle multiplication
+	if (multiplicationPattern.test(amount)) {
+		return multiplyExpression(amount);
+	}
+
+	// If it's a decimal or whole number, return it directly
+	if (decimalPattern.test(amount) || wholeNumberPattern.test(amount)) {
+		return Number(amount);
+	}
+
+	// If no pattern matches, return NaN or throw an error as appropriate
+	return NaN;
+}
